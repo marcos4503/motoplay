de614396b5b83e0d06d704ec278beb02c37ce0b8
Marcos Tomaz <marcos1tomaz2@gmail.com>
2024-09-11 23:56:09 -0300

    Pages update

--- Repository-Pages/current-clean-motoplay-script.sh
+++ Repository-Pages/current-clean-motoplay-script.sh
@@ -5,4 +5,9 @@ cd ~
 rm -r Motoplay
 rm .local/share/applications/Motoplay.desktop
 rm .local/share/applications/Motoplay Installer.desktop
+cp .config/wayfire.ini .config/wayfire-autobackup.ini
+sed -i 's/motoplay = \/home\/rpi\/Motoplay\/App\/Motoplay.Desktop/ /g' .config/wayfire.ini
+sed -i 's/\[autostart\]/ /g' .config/wayfire.ini
 echo "Motoplay Uninstallation Finished!"
+echo "Rebooting in 10 Seconds..."
+sleep 10
